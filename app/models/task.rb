955ee945feb7cdb9efdccd3e08d13576ba906da3
Andy Park <andyxpark@gmail.com>
2021-02-08 01:50:38 -0500

    added tests and code to tasks

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,13 @@
 class Task < ApplicationRecord
+	belongs_to :child
+	belongs_to :task
+	# relationships
+	has_many :chores
+	has_many :children, through: :chores
+	# validations
+	validates_presence_of :name
+	validates_numericality_of :points, only_integer: true, greater_than: 0
+	# scopes
+	scope :alphabetical, -> { order('name') }
+	scope :active,       -> { where(active: true) }
 end
